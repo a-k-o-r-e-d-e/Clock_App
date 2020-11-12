de3b5ae9f52adb925a6d38a087f584fab60e34ca
Lone-Wolf17 <53178226+Lone-Wolf17@users.noreply.github.com>
2020-11-12 12:00:11 +0100

    ** Built & Animated Clock UI using Custom Painter

---
+++ lib/constants/myColors.dart
@@ -0,0 +1,7 @@
+import 'dart:ui';
+
+class MyColors {
+
+  static final Color offWhite = Color(0xFFEAECFF);
+
+}
--- lib/views/home.dart
+++ lib/views/home.dart
@@ -1,3 +1,4 @@
+import 'package:clock_app/views/clock_view.dart';
 import 'package:flutter/material.dart';
 
 class Home extends StatelessWidget {
@@ -5,7 +6,9 @@ class Home extends StatelessWidget {
   Widget build(BuildContext context) {
     return Scaffold(
       body: Container(
+        alignment: Alignment.center,
         color: Color(0xFF2D2F41),
+        child: ClockView(),
       ),
     );
   }
